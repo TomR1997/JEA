878311409c5126254b9a478e934b683eceb3a6e2
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-03-09 14:09:56 +0100

    RoleServiceTest

--- Kwetter/src/main/java/service/RoleService.java
+++ Kwetter/src/main/java/service/RoleService.java
@@ -6,9 +6,13 @@
 package service;
 
 import dao.RoleDAO;
+import dao.exceptions.NonExistingEntryException;
 import domain.Role;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import service.exceptions.NonExistingRoleException;
 
 /**
  *
@@ -16,10 +20,32 @@ import javax.inject.Inject;
  */
 @Stateless
 public class RoleService {
+
     @Inject
     private RoleDAO roleDao;
-    
-    /*public Role findRole(String name){
-        return roleDao.find(name);
-    }*/
+
+    public void setRoleDao(RoleDAO roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    public Role findRole(String name) throws NonExistingRoleException {
+        try {
+            return roleDao.find(name);
+        } catch (NonExistingEntryException ex) {
+            Logger.getLogger(RoleService.class.getName()).log(Level.SEVERE, null, ex);
+            throw new NonExistingRoleException();
+        }
+    }
+
+    public void saveRole(Role role) throws NonExistingRoleException {
+        if (role == null) {
+            throw new NonExistingRoleException();
+        }
+        try {
+            roleDao.save(role);
+        } catch (NonExistingEntryException ex) {
+            Logger.getLogger(RoleService.class.getName()).log(Level.SEVERE, null, ex);
+            throw new NonExistingRoleException();
+        }
+    }
 }
---
+++ Kwetter/src/test/java/service/RoleServiceTest.java
@@ -0,0 +1,49 @@
+package service;
+
+import dao.RoleDAO;
+import dao.exceptions.NonExistingEntryException;
+import domain.Role;
+import domain.RoleName;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.runners.MockitoJUnitRunner;
+import service.exceptions.NonExistingRoleException;
+
+/**
+ *
+ * @author Tomt
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RoleServiceTest {
+
+    @Mock
+    private RoleDAO roleDao;
+    @InjectMocks
+    private RoleService roleService;
+    private Role role;
+
+    @Before
+    public void setUp() {
+        roleService = new RoleService();
+        roleService.setRoleDao(roleDao);
+        role = new Role(RoleName.MODERATOR.toString());
+    }
+
+    @Test
+    public void findRoleTest() throws NonExistingEntryException, NonExistingRoleException {
+        when(roleDao.find(role.getName())).thenReturn(role);
+        Role r = roleService.findRole(role.getName());
+        assertSame(role, r);
+    }
+
+    @Test(expected = NonExistingRoleException.class)
+    public void exceptionFindRoleTest() throws NonExistingEntryException, NonExistingRoleException {
+        when(roleDao.find(role.getName())).thenThrow(new NonExistingEntryException());
+        roleService.findRole(role.getName());
+    }
+}
