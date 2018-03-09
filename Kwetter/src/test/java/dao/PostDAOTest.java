6454551ede39defc3f04488a11e77cf69c24472c
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-03-09 13:56:54 +0100

    PostDaoTest

--- Kwetter/src/test/java/dao/PostDAOTest.java
+++ Kwetter/src/test/java/dao/PostDAOTest.java
@@ -5,6 +5,10 @@
  */
 package dao;
 
+import dao.exceptions.EmptyListException;
+import dao.exceptions.NonExistingEntryException;
+import domain.Post;
+import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -20,7 +24,7 @@ import static org.junit.Assert.*;
  * @author Tomt
  */
 public class PostDAOTest {
-    
+
     private EntityManager em;
     private EntityManagerFactory emf;
     private PostDAO postDao;
@@ -30,6 +34,45 @@ public class PostDAOTest {
         emf = Persistence.createEntityManagerFactory("KwetterPU");
         em = emf.createEntityManager();
         postDao = new PostDAO(em);
+    }
+
+    @Test
+    public void findPostTest() throws NonExistingEntryException {
+        em.getTransaction().begin();
+        Post post = postDao.find(1L);
+        assertSame(post, postDao.find(1L));
+        em.close();
+    }
+
+    @Test
+    public void createPostTest() throws NonExistingEntryException, EmptyListException {
+        em.getTransaction().begin();
+        int allPosts = postDao.getAll().size();
+        Post post = new Post("Hey all!", new Date());
+        postDao.save(post);
+        assertEquals(allPosts + 1, postDao.getAll().size());
+        em.close();
+    }
+
+    @Test(expected = NonExistingEntryException.class)
+    public void nonExistingPostExceptionTest() throws NonExistingEntryException {
+        em.getTransaction().begin();
+        postDao.find(-1L);
+        em.close();
+    }
+    
+    @Test
+    public void getLatestPostsTest() throws EmptyListException{
+        em.getTransaction().begin();
+        assertTrue(!postDao.getLatestPosts(1L).isEmpty());
+        em.close();
+    }
+    
+    @Test
+    public void getTimelineTest() throws EmptyListException{
+        em.getTransaction().begin();
+        assertTrue(!postDao.getTimeline(1L).isEmpty());
+        em.close();
     }
 
 }
